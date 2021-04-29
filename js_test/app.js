45ccf4bccc637aa09f47a1846eba9c66bc523e26
Eric Van Meter <ericvanmeter57@gmail.com>
2021-04-28 23:01:40 -0500

    end of day push

--- js_test/app.js
+++ js_test/app.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+if (phrase === 'stop') {
+    console.log('red');
+} else if (phrase === 'slow') {
+    console.log('yellow');
+} else if (phrase === 'go') {
+    console.log('green');
+} else {
+    console.log('purple');
+}

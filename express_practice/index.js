46711c5f66d6c085307f36e8d8b1b9fc1c97b53c
Eric Van Meter <ericvanmeter57@gmail.com>
2021-05-05 11:03:02 -0500

    ejs routes

--- express_practice/index.js
+++ express_practice/index.js
@@ -1,35 +1,37 @@
 const express = require("express");
 const app = express();
 
+app.set('view engine', 'ejs')
+
 // app.use((req, res) => {
 //     console.log("NEW REQUEST");
 //     res.send("<h1>This is my webpage!<h1>")
 // })
 
 app.get('/', (req, res) => {
-    res.send('This is the homepage!')
+    res.render('home')
 })
 
-app.get('/r/:subreddit', (req, res) => {
-    const { subreddit } = req.params;
-    res.send(`<h1>Browsing the ${subreddit} subreddit!!!`)
-})
+// app.get('/r/:subreddit', (req, res) => {
+//     const { subreddit } = req.params;
+//     res.send(`<h1>Browsing the ${subreddit} subreddit!!!`)
+// })
 
-app.post('/cats', (req, res) => {
-    res.send('Post req')
-})
+// app.post('/cats', (req, res) => {
+//     res.send('Post req')
+// })
 
-app.get('/cats', (req, res) => {
-    res.send('MEOW!!')
-})
+// app.get('/cats', (req, res) => {
+//     res.send('MEOW!!')
+// })
 
-app.get('/dogs', (req, res) => {
-    res.send('BARK!!')
-})
+// app.get('/dogs', (req, res) => {
+//     res.send('BARK!!')
+// })
 
-app.get('*', (req, res) => {
-    res.send('I dont know that path')
-})
+// app.get('*', (req, res) => {
+//     res.send('I dont know that path')
+// })
 
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000");

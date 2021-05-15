c2307c09c002e37199dcc68c31645d8d35d5f323
Eric Van Meter <ericvanmeter57@gmail.com>
2021-05-15 14:25:33 -0500

    testing big o problems

---
+++ index.js
@@ -0,0 +1,32 @@
+// function boo(n) {
+//     for (let i = 0; i < n.length; i++) {
+//         console.log('Boo!');
+//     }
+// }
+
+// boo(5)
+
+// function arrayOfHiNTimes(n) {
+//     let hiArray = [];
+//     for (let i = 0; i < n; i++) {
+//         hiArray[i] = 'hi';
+//     }
+//     return hiArray;
+// }
+
+// console.log(arrayOfHiNTimes(5));
+
+const arr = [{
+    tweet: 'hi',
+    date: 2012
+    }, {
+    tweet: 'my',
+    date: 2015
+    }, {
+    tweet: 'teddy',
+    date: 2021
+}];
+
+console.log(arr[0].date);
+arr[0];
+arr[arr.length-1];
